255ba65054e09931ddb33139fa16d20075ef6cc6
Daniel Skowroński <daniel@dsinf.net>
2021-05-07 21:28:14 +0200

    CLI

--- XpgSummonerControl.py
+++ XpgSummonerControl.py
@@ -18,7 +18,7 @@ class XpgSummonerControl:
         for i in range(0,3):
           self.hidraw.send_feature_report([0x07, 0xa6, 0x00, 0x4d, 0x00, 0x00, 0x00, profileID])
           self.hidraw.get_feature_report(0,32)
-          
+
     def setColorForProfile(self, profileID, red, green, blue):
         if not (profileID in range(0,5)):
             raise Exception("wrong profile ID")
@@ -36,13 +36,25 @@ class XpgSummonerControl:
           self.hidraw.get_feature_report(0,32)
 
 
-xsc = XpgSummonerControl()
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description=
+        'XpgSummonerControl - control ADATA XPG Summoner keyboard backlight (RGB) and profiles')
+    subparsers = parser.add_subparsers(help='commands',dest='cmd')
 
-#for p in range(0,5):
-#    print(p)
-#    xsc.selectProfile(p)
-#    sleep(1)
+    switchProfile_parser = subparsers.add_parser('switch_profile')
+    switchProfile_parser.add_argument('--profile', type=int, choices=range(0,5),required=True, help='profile number; F1 is 0, F2 is 1, ...')
 
-xsc.setColorForProfile(4, 0,0,255)
-sleep(2)
-xsc.setColorForProfile(4, 255,255,255)
+    setColor_parser = subparsers.add_parser('set_color')
+    setColor_parser.add_argument('--profile', type=int, choices=range(0,5),  required=True, help='profile number; F1 is 0, F2 is 1, ...')
+    setColor_parser.add_argument('--red',     type=int, choices=range(0,256),required=True, help='red value')
+    setColor_parser.add_argument('--green',   type=int, choices=range(0,256),required=True, help='green value')
+    setColor_parser.add_argument('--blue',    type=int, choices=range(0,256),required=True, help='blue value')  
+
+    args = parser.parse_args()
+    xsc = XpgSummonerControl()
+
+    if args.cmd=='switch_profile':
+        xsc.selectProfile(args.profile)
+    if args.cmd=='set_color':
+        xsc.setColorForProfile(args.profile, args.red, args.green, args.blue)
